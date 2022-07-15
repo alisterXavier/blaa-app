6b8b6e2f4629bd4aacf47300ea3f0070ce77660c
Alister Xavier Sequeira <67282719+Alister153@users.noreply.github.com>
2022-07-15 22:31:29 +0400

    server check

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use(express.json());
 
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname1, '/client/build')))
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname1, 'client', 'build', 'index.html'))
   })
 }else{
